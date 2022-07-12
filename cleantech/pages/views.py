2744433e3626c9701b2754e9287a1ce10ca1430c
Burak <eselik.burak@gmail.com>
2022-07-12 18:22:18 +0300

    Home page, and about page created

--- cleantech/pages/views.py
+++ cleantech/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'pages/home.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'pages/about.html', context)
