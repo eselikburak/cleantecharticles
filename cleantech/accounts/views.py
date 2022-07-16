0745669c2c8f6aaccdd5b408ac7a1dc9c854f7d1
Burak <eselik.burak@gmail.com>
2022-07-16 08:33:46 +0300

    Added a new message if a user enter wrong email or password

--- cleantech/accounts/views.py
+++ cleantech/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.add_message(request, messages.INFO, 'Password or Username invalid!')
+        return redirect('home')
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
