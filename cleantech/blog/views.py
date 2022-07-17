30b1850ccaabc20d57d3704b4fe16a76e8939051
Burak <eselik.burak@gmail.com>
2022-07-17 11:35:53 +0300

    A blog author can post update

--- cleantech/blog/views.py
+++ cleantech/blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import PostForm
 from .models import Post, Category
 from django.contrib.auth.decorators import login_required
 
 def blog(request):
-    posts = Post.objects.all().order_by('-created_date')
+    posts = Post.objects.filter(is_available=True).order_by('-created_date')
     categories = Category.objects.all()
     context = {
         'posts': posts,
@@ -13,7 +15,7 @@ def blog(request):
     return render(request, 'blog/blog.html', context)
 
 def blog_as_category(request, category_slug):
-    posts = Post.objects.filter(categories__slug=category_slug)
+    posts = Post.objects.filter(is_available=True, categories__slug=category_slug)
     categories = Category.objects.all()
     all_posts = Post.objects.all().order_by('-created_date')
     context = {
@@ -24,7 +26,7 @@ def blog_as_category(request, category_slug):
     return render(request, 'blog/blog.html', context)
 
 def post_detail(request, post_slug):
-    post = Post.objects.get(slug=post_slug)
+    post = Post.objects.get(is_available=True, slug=post_slug)
     context = {
         'post': post,
         'the_post_categories': post.categories.all()
@@ -35,9 +37,20 @@ def post_detail(request, post_slug):
 @login_required
 def post_update(request, pk):
     post = Post.objects.get(pk=pk)
-    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish()
+
+            return redirect('post_detail', post_slug=post.slug)
+    #categories = Category.objects.all()
+    else: 
+        form = PostForm(instance=post)
     context = {
-        'post': post,
-        'categories': categories
+        'form': form,
     }
     return render(request, 'blog/post_update.html', context)
