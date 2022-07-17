8072a2997d2ef6fcd18e94b44fa6b5ea8ae669bd
Burak <eselik.burak@gmail.com>
2022-07-17 18:51:14 +0300

    redesigned PostForm

--- cleantech/blog/forms.py
+++ cleantech/blog/forms.py
@@ -1,8 +1,28 @@
+
 from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.widgets.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Post Title',
+    }))
+    content = forms.CharField(widget=forms.widgets.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Post Content',
+        'rows': '16',
+    }))
+    image = forms.ImageField(widget=forms.widgets.ClearableFileInput(attrs={
+        'class':'form-control',
+    }))
+    is_available = forms.BooleanField(widget=forms.CheckboxInput(attrs={
+        'class': 'form-check-input',
+    }), required=False)
 
     class Meta:
         model = Post
         fields = ['title', 'content', 'image', 'is_available', 'categories']
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['categories'].widget.attrs.update({'class': 'form-select'})
