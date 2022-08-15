d81c60252221001e43e9129fe92383baf8de3c80
Burak Eselik <eselik.burak@gmail.com>
2022-08-15 09:15:16 +0300

    Re-edit search results style and attitudes

--- cleantech/static/js/search.js
+++ cleantech/static/js/search.js
@@ -1,28 +1,38 @@
 
-$(document).ready( function() {
+$(document).ready(function () {
+    // Listen searchbox
     $("#searchbox").on("input", function () {
-        $("#search_results").html('')
-        $.ajax({
-            url: "/blog/search/",
-            type: "get",
-            data: {
-                searchbox_text: this.value
-            },
-            success: function (responce) {
-                results = JSON.parse(responce.search_results);
-                $("#searchcard").removeClass("d-none");
-                if (results == false) {
-                    // Do nothing or add 
-                    $("#searchcard").addClass("d-none");
-                } else {
-                    for (let i = 0; i < results.length; i++) {
-                        $("#search_results").html($("#search_results").html() + "<a class='list-group-item'>"+ results[i].fields.title + "</a>");
-                        $("#search_results > a:last").attr("href", '/blog/post/' + results[i].fields.slug);
-                    };
-                };
-            }
-        });
+        if (this.value.length == 1 || this.value.length % 3 == 0) {
+            $("#search_results").html('')
+            $.ajax({
+                url: "/blog/search/",
+                type: "get",
+                data: {
+                    searchbox_text: this.value
+                },
+                success: function (responce) {
+                    results = JSON.parse(responce.search_results);
+                    $("#searchcard").css("display", "block");
+                    if (results == false) {
+                        // 
+                        let x = document.getElementById("searchbox").value;
+                        if (x.length < 1) {
+                            $("#searchcard").css("display", "none");
 
+                        } else {
+                            $("#search_results").html('')
+                            let msg = "No Result!"
+                            $("#search_results").html($("#search_results").html() + "<p class='list-group-item text-warning'> - " + msg + "</p>");
+                        };
+                    } else {
+                        for (let i = 0; i < results.length; i++) {
+                            $("#search_results").html($("#search_results").html() + "<a class='list-group-item'> - " + results[i].fields.title + "</a>");
+                            $("#search_results > a:last").attr("href", '/blog/post/' + results[i].fields.slug);
+                        };
+                    };
+                }
+            });
+        };
     });
 
 });
